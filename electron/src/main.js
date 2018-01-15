6f57ae85ae249b28fc72d297b615b655eae72b31
Thibaut Renoncourt <thibaut.renoncourt@gmail.com>
2018-01-15 08:50:18 +0100

    fix(json-list): add renderer callback

--- electron/src/main.js
+++ electron/src/main.js
@@ -130,11 +130,6 @@ app.on('window-all-closed', () => {
     app.quit()
   }
 })
-app.on('login', (event, webContents, request, authInfo, callback) => {
-  console.log(request)
-  event.preventDefault()
-  callback('simon', 'Penis!!')
-})
 app.on('activate', () => {
   if (mainWindow === null) {
     createWindow()
